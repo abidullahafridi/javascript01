5af2c95057498be6a8aee06508e313dd438c9492
abidullahafridi <116450166+abidullahafridi@users.noreply.github.com>
2022-10-31 16:55:44 +0500

    this is javascript

---
+++ app.js
@@ -0,0 +1,42 @@
+//alert("thank for your input")
+//var a=50
+//var b=30
+//var c = a + b
+//var c = a * b
+//var c = a - b
+//var c = a / b
+//alert(c) 
+
+//var a = 30;
+//var b = 60;
+//var c = 80;
+//var e  = 40;
+//var f = a - b; 
+//alert(c)
+
+//var a = 66
+//var b = 42
+//var c = 34
+//var d = 34
+//var e = (a - b) * c 
+//alert(e)
+
+//var a = 55
+//var b = 67
+//var c = 85
+//var d =  c * (a + b)
+//alert(d)
+
+//var a = 55;
+//var b = 99;
+//a++;
+//b--;
+//console.log(a);
+//console.log(b);
+
+
+//var text = hello "Abid"
+ //var username = "Abid ullah"
+ //var dot = "."
+ //var total = text + username + dot
+ //alert(total);
